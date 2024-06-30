bdaabe6ea06a10c965b76565562c97b337aa5696
Rahul Gupta <rahul7668gupta@gmail.com>
2024-06-30 15:37:09 +0530

    all projects

--- app/utils/projectArray.ts
+++ app/utils/projectArray.ts
@@ -1,9 +1,33 @@
 export const dummyProjects = [
     {
-        name: "Enjoy 0kcal",
-        fid: 3307,
-        wallet: "0xAF23D4e2108e2098d7C4ff56e329e36fef358253",
-        imageName: "enjoy0kcal.png"
+        name: "Sidebets",
+        fid: 1265,
+        wallet: "0xab14023979a34b4abb17abd099a1de1dc452011a",
+        imageName: "sidebets.png"
+    },
+    {
+        name: "TokeMail",
+        fid: 486287,
+        wallet: "0x3a2b4a32cf84a0fa5d3fdcff818ebe9aa2bd3c94",
+        imageName: "tokemail.png"
+    },
+    {
+        name: "Base Wave",
+        fid: 474185,
+        wallet: "0x8525f916f69A8465e4Ef3651A73Abf72fB4E4DC8",
+        imageName: "basewave.png"
+    },
+    {
+        name: "Zoracart",
+        fid: 17979,
+        wallet: "0x22b2DD2CFEF2018D15543c484aceF6D9B5435863",
+        imageName: "zoracart.png"
+    },
+    {
+        name: "Phools",
+        fid: 344145,
+        wallet: "0xf8c80ebf80ff0c17bdfbc08a91e64a890aae8e3c",
+        imageName: "phools.png"
     },
     {
         name: "Based Coffee",
@@ -24,11 +48,6 @@ export const dummyProjects = [
         imageName: "farmix.png"
     },
     {
-        name: "Farcaster Actions",
-        fid: 9369,
-        imageName: "fc-actions.png"
-    },
-    {
         name: "Rolodex",
         fid: 11155,
         wallet: "0x0674ab75Cbf5f3165Fe70BE116Bed13DF134664A",
@@ -41,12 +60,6 @@ export const dummyProjects = [
         imageName: "higherboard.png"
     },
     {
-        name: "Farview",
-        fid: 244416,
-        wallet: "0x3039e4a4a540F35ae03A09f3D5A122c49566f919",
-        imageName: "farview.png"
-    },
-    {
         name: "Coven Classics",
         fid: 4027,
         wallet: "0x3a2FaC2AB3cf4bc342910EAC744CC68C8b9a69bf",
@@ -57,5 +70,22 @@ export const dummyProjects = [
         fid: 1350,
         wallet: "0xbC7B5142e390BBaABF8826dAE7E1ef9C609Df327",
         imageName: "superwarp.png"
-    }
+    },
+    {
+        name: "Farcaster Actions",
+        fid: 9369,
+        imageName: "fc-actions.png"
+    },
+    {
+        name: "Farview",
+        fid: 244416,
+        wallet: "0x3039e4a4a540F35ae03A09f3D5A122c49566f919",
+        imageName: "farview.png"
+    },
+    {
+        name: "Enjoy 0kcal",
+        fid: 3307,
+        wallet: "0xAF23D4e2108e2098d7C4ff56e329e36fef358253",
+        imageName: "enjoy0kcal.png"
+    },
 ];
